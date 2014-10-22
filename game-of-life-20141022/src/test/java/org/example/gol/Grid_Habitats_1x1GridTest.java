647d816c18dae873783096ea3bbd1e32a658433c
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2014-10-22 22:57:14 +0200

    removed duplication in other grid test⁄

--- game-of-life-20141022/src/test/java/org/example/gol/Grid_Habitats_1x1GridTest.java
+++ game-of-life-20141022/src/test/java/org/example/gol/Grid_Habitats_1x1GridTest.java
@@ -1,6 +1,5 @@
 package org.example.gol;
 
-import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.util.List;
@@ -9,12 +8,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 
 public class Grid_Habitats_1x1GridTest {
+    private final GridBuilder grid = new GridBuilder().width(1);
 
     @Test
     public void returnOneHabitat() throws Exception {
-        List<Cell> cells = Lists.newArrayList();
-        cells.add(Cell.x);
+        grid.add(Cell.x);
 
-        assertThat(new Grid(cells, 1).habitats(), hasSize(1));
+        assertThat(habitats(), hasSize(1));
     }
+
+    private List<Habitat> habitats() {
+        return grid.grid().habitats();
+    }
+
 }
