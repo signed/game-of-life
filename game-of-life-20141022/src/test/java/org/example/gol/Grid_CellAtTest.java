40e985de84d55a7fa26d606ff9faa55340875758
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2014-10-22 22:11:45 +0200

    Refactored towards a cell setup that reflects the layout of the grid

--- game-of-life-20141022/src/test/java/org/example/gol/Grid_CellAtTest.java
+++ game-of-life-20141022/src/test/java/org/example/gol/Grid_CellAtTest.java
@@ -3,8 +3,11 @@ package org.example.gol;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.List;
 
+import static org.example.gol.Cell.*;
+import static org.example.gol.Cell.o;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -14,7 +17,7 @@ public class Grid_CellAtTest {
 
     @Test
     public void placeTheFirstCellAt_0_0() throws Exception {
-        cells.add(Cell.x);
+        cells.add(x);
 
         assertThat(grid().getAt(0, 0).alive(), is(true));
     }
@@ -22,18 +25,21 @@ public class Grid_CellAtTest {
     @Test
     public void placeTheTheSecondCellAt_1_0_inAGridWithWidth_2() throws Exception {
         width = 2;
-        cells.add(Cell.o);
-        cells.add(Cell.x);
+        add(o, x);
 
         assertThat(grid().getAt(1, 0).alive(), is(true));
     }
 
     @Test
     public void placeTheTheSecondCellAt_0_1() throws Exception {
-        cells.add(Cell.o);
-        cells.add(Cell.x);
+        cells.add(o);
+        cells.add(x);
 
         assertThat(grid().getAt(0, 1).alive(), is(true));
+    }
+
+    private void add(Cell ... row) {
+        Collections.addAll(cells, row);
     }
 
     private Grid grid() {
