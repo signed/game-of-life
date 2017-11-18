fdbc39190d3037da8aeb1a4ab3a3931b0c6b9445
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-11-18 22:41:16 +0100

    coordinate, world, evolve

---
+++ game-of-life-20171118/src/main/java/org/example/gol/Coordinates.java
@@ -0,0 +1,36 @@
+package org.example.gol;
+
+class Coordinates {
+    public static Coordinates xy(int x, int y) {
+        return new Coordinates(x, y);
+    }
+
+    private final int x;
+    private final int y;
+
+    private Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "/" + y + ')';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+}
---
+++ game-of-life-20171118/src/test/java/org/example/gol/GameOfLifeTest.java
@@ -0,0 +1,75 @@
+package org.example.gol;
+
+import org.junit.Test;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.example.gol.Coordinates.xy;
+
+public class GameOfLifeTest {
+
+    @Test
+    public void twoCoordinatesAreEqualIfThereXandYvaluesMatch() throws Exception {
+        Coordinates coordinate = xy(0, 0);
+        Set<Coordinates> coordinates = new LinkedHashSet<>();
+        coordinates.add(coordinate);
+
+        assertThat(coordinates).contains(xy(0, 0)).doesNotContain(xy(1, 1));
+    }
+
+    private final LinkedHashSet<Coordinates> aliveCells = new LinkedHashSet<>();
+
+    @Test
+    public void theWorldProvidesInformationIfACellAtGivenCoordinatesIsAlive() throws Exception {
+        cellAliveAt(xy(0, 0));
+
+        assertThat(world().isAliveAt(xy(0, 0))).isTrue();
+        assertThat(world().isAliveAt(xy(1, 1))).isFalse();
+    }
+
+    @Test
+    public void cellWithoutNeighboursDies() throws Exception {
+        cellAliveAt(xy(0, 0));
+
+        assertThat(world().evolve().isAliveAt(xy(0,0))).isFalse();
+    }
+
+    @Test
+    public void cellWithTwoNeighboursStaysAlive() throws Exception {
+        cellAliveAt(xy(-1, -1));
+        cellAliveAt(xy(0, 0));
+        cellAliveAt(xy(1, 1));
+
+        assertThat(world().evolve().isAliveAt(xy(0,0))).isTrue();
+    }
+
+    private boolean cellAliveAt(Coordinates coordinates) {
+        return aliveCells.add(coordinates);
+    }
+
+    private World world() {
+        return new World(aliveCells);
+    }
+
+    private class World {
+        private Set<Coordinates> aliveCells;
+
+        public World(Set<Coordinates> aliveCells) {
+            this.aliveCells = aliveCells;
+        }
+
+        public boolean isAliveAt(Coordinates coordinates) {
+            return aliveCells.contains(coordinates);
+        }
+
+        public World evolve() {
+            Set<Coordinates> set = new LinkedHashSet<>();
+            if (aliveCells.size() == 3) {
+                set.add(xy(0, 0));
+            }
+            return new World(set);
+        }
+    }
+}
